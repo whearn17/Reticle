c371691117e6fa4a0b88eb150683f3b262743446
William Hearn <williamhearn17@gmail.com>
2025-04-11 00:19:05 +0000

    did some stuff

--- reticle.py
+++ reticle.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import web_traffic_replay
 import reticle_arguments
+import reticle_url_utils
 import http_log_reader
 import pathlib
 import json
+import time
 
 
 if __name__ == "__main__":
@@ -12,15 +14,29 @@ if __name__ == "__main__":
 
     args = reticle_arguments.parse_arguments()
 
-    site = args.site
+    site = args.url
 
-    screenshot_save_path = args.output_path
+    screenshot_save_path = args.output
     screenshot_save_path = pathlib.Path(screenshot_save_path).resolve()
 
-    url_list = http_log_reader.process_log_file("./samples/apache2.log")
+    log_file_path = args.log
+    log_file_path = pathlib.Path(log_file_path).resolve()
+
+    uri_list = http_log_reader.process_log_file(log_file_path)
+
+    ignore_list = args.ignore
+
+    if ignore_list:
+        uri_list = reticle_url_utils.filter_uri_list(uri_list, ignore_list)
+
+    modify_params = args.modify_params
+
+    if modify_params:
+        for param in modify_params:
+            key, value = param.split("=")
+            uri_list = [reticle_url_utils.modify_query_param(uri, key, value) for uri in uri_list]
 
     driver = webdriver.Chrome()
-
     driver.get(site)
 
     cookies = None
@@ -35,6 +51,10 @@ if __name__ == "__main__":
 
     driver.refresh()
 
-    for count, url in enumerate(url_list):
+    for count, uri in enumerate(uri_list):
 
-        web_traffic_replay.replay_url(f"{site}{url}", driver, screenshot_save_path=f"{screenshot_save_path}/screenshot_{count}.png")
+        full_url = f"{site}{uri}"
+
+        web_traffic_replay.replay_url(full_url, driver, screenshot_save_path=f"{screenshot_save_path}/screenshot_{count}.png")
+
+        time.sleep(3)
